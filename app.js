7a5f2365d6cb4bf2019a1904a42d7aaf200b2466
Alejandro102939 <moralesluis200504gmail.com>
2023-10-22 13:39:34 -0500

    Primer commit

--- app.js
+++ app.js
@@ -1,28 +1,43 @@
+// Metodo para llamar el archivo Colors
 require('colors');
 
+const { green } = require('colors');
+// Metodo para llamar la carpeta `proy_modules` y llamar el archivo `math.js`
 const math = require('./proy_modules/math.js')
 
+// Se utiliza main para una funcion asincronica, imprime "hola SENA CBA"
 const main = async () => {
-    console.log('hola SENA CBA\n'.red);
+    console.log(`**************`.green);
+    console.log('hola SENA CBA*'.bgRed);
+    console.log(`**************`.green);
 
+    // Se crea un arreglo llamado `numeros` luego se realiza un bucle `for` dentro del bucle se invierten los numeros
     const numeros = [1, 2, 800, 912, 1000, 1283, 2811];
     for (const numero of numeros) {
         const invertidoComoNumero = math.invertirNumero(numero);
         const invertirComoCadena = math.invertirNumeroComoTexto(numero);
-        console.log("El numero '%s' invertido como numero, y '%S' como cadena",
-                    numero, invertidoComoNumero, invertirComoCadena);
+        console.log(`*********************************************************************`.red);
+        console.log("El numero '%s' invertido como numero, y '%S' como cadena", 
+        numero, invertidoComoNumero, invertirComoCadena);
+        console.log(`*********************************************************************`.red);
     }
 
+    // Se crea un arreglo llamdo `textos` luego se realiza un bucle `for` dentro del bucle se invierten los textos 
     const textos = ['Hola Sena', 'Ficha 2798618', 'Analisis y desarrollo de software']
     for(const texto of textos){
         const textoInvertido = math.invertirTexto(texto);
+        console.log(`**************************************************************************************************`.green);
         console.log("El texto '%s' se invierte como '%s'", texto, textoInvertido);
+        console.log(`**************************************************************************************************`.green);
     }
 
+    //Se crea un arreglo llamado `datos` luego se realiza un bucle `for` luego se realiza una conversion segun el tipo de dato 
     const datos = [1234567, 'ADSO 2798618', [2798618, 'ADSO']]
     for(let dato of datos){
         const datoConvertido = math.conversionDatos(dato);
+        console.log(`*********************************************************************`.yellow);
         console.log(`El resultado de conversion es: ${datoConvertido}`);
+        console.log(`*********************************************************************`.yellow);
     }
 }
 
--- proy_modules/math.js
+++ proy_modules/math.js
@@ -1,5 +1,7 @@
+// Funciona para almacenar funciones y operaciones matematicas
 const math = {};
 
+// Funcion que toma un numero como argumento y devuelve su version invertida y toma un bucle `while` para calcular el numero invertido 
 const invertirNumero = numero => {
     let invertido = 0;
     while(numero !=0) {
@@ -9,11 +11,14 @@ const invertirNumero = numero => {
     return invertido
 };
 
+// Funcion que toma un numero y lo convierte en cadena de texto, invierte esta cadena y la vuelve a unir
 const invertirNumeroComoTexto = numero => {
     const numeroInvertidoComoTexto = numero.toString().split("").reverse().join("");
     return numeroInvertidoComoTexto;
 };
 
+
+// Funcion que toma una cadena de texto y la divide en un arreglo de caracteres, e invierte el arreglo 
 const invertirTexto = texto => {
     const splitText = texto.split('');
     const reversedText = splitText.reverse();
@@ -21,11 +26,14 @@ const invertirTexto = texto => {
     return joinedText;
 };
 
+
+// Funcion que toma un arreglo y lo invierte utilizando el metodo `reverse()`
 const invertirArreglo = arreglo => {
     const reverseObject = arreglo.reverse();
     return reverseObject;
 };
 
+// Funcion que toma un dato y determina su tipo, luego verifica si el dato es vacio o incorrecto y si no lo es, utiliza un bloque `switch` para aplicar la funcion correspondiente 
 const conversionDatos = data => {
     let dataType = typeof data;
     let dataVar;
@@ -33,7 +41,9 @@ const conversionDatos = data => {
     if(data === '' || data === null || data === undefined || data.lenght === 0) {
         dataVar = 'El valor ingresado esta vacio o incorrecto';
     }else {
-        console.log(`El valor ingresado es de tipo: ${dataType}, con un valor de: ${data}`);
+        console.log(`*********************************************************************`.cyan);
+        console.log(`El valor ingresado es de tipo: ${dataType}, con un valor de: ${data}`.bgCyan);
+        console.log(`*********************************************************************`.cyan);
         switch(dataType) {
             case 'number':
                 dataVar = invertirNumero(data);
@@ -51,10 +61,12 @@ const conversionDatos = data => {
     return dataVar;
 }
 
+// Estas lineas añaden las funciones definidas anteriormente al objeto `math`
 math.invertirNumero = invertirNumero;
 math.invertirNumeroComoTexto = invertirNumeroComoTexto;
 math.invertirTexto = invertirTexto;
 math.invertirArreglo = invertirArreglo;
 math.conversionDatos = conversionDatos;
 
+// Esta linea exporta el objeto `math` para que pueda ser utilizado por otros modulos de Node.js
 module.exports = math;
